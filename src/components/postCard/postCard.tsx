1480efbc16b06d483736b4566770312da3541f1a
Luis Simosa <simosa37@gmail.com>
2023-03-13 10:39:24 -0300

    postCard design implemented

--- src/components/postCard/postCard.tsx
+++ src/components/postCard/postCard.tsx
@@ -25,14 +25,14 @@ const PostCard = () => {
             return (
               <li
                 key={post.id}
-                className="my-8 mx-3 grid grid-rows-2 border bg-slate-100 p-2"
+                className="my-8 mx-3 grid grid-rows-2 border bg-slate-100 p-2 h-64"
               >
-                <div className="row-start-2 flex flex-col">
-                  <div className="flex gap-2">
-                    <p className="">{post.tags[0]}</p>
-                    <p className="">{post.createAt.toLocaleDateString()}</p>
+                <div className="row-start-2 flex flex-col self-end">
+                  <div className="flex gap-2 mb-2 items-center">
+                    <p className="uppercase bg-yellow-300 px-2 py-[1px] rounded text-sm mr-2">{post.tags[0]}</p>
+                    <p className="text-xs">{post.createAt.toLocaleDateString()}</p>
                   </div>
-                  <p className="">{post.title}</p>
+                  <p className="text-xl font-semibold">{post.title}</p>
                 </div>
               </li>
             );
