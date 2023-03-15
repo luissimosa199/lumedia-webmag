009e29539d21f9b9d79fd19b3bba710da866fc32
Luis Simosa <simosa37@gmail.com>
2023-03-15 12:50:50 -0300

    deleted the api call on Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,7 @@
 import { type NextPage } from "next";
 import PostCard from "~/components/postCard/postCard";
-import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const { data, error, isLoading } = api.post.getLatest.useQuery();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return (
-      <>
-        <div>{error.message}</div>;
-      </>
-    );
-  }
 
   return (
     <>
