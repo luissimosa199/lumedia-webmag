483deacc14544c5113880deebbcaf8d511d092d4
Luis Simosa <simosa37@gmail.com>
2023-03-17 10:10:49 -0300

    header links text now change color on hover

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -8,10 +8,10 @@ const NavBar = () => {
         <li className="border border-collapse px-3 leading-[3.5rem] cursor-pointer hover:opacity-75">
           Popular
         </li>
-        <li className="border border-collapse px-3 leading-[3.5rem] cursor-pointer border-b-4 border-b-yellow-300 hover:opacity-75">
+        <li className="border border-collapse px-3 leading-[3.5rem] cursor-pointer border-b-4 border-b-yellow-300 hover:opacity-75 hover:text-yellow-700">
           Categoria 1
         </li>
-        <li className="border border-collapse px-3 leading-[3.5rem] cursor-pointer border-b-4 border-b-emerald-600 hover:opacity-75">
+        <li className="border border-collapse px-3 leading-[3.5rem] cursor-pointer border-b-4 border-b-emerald-600 hover:opacity-75 hover:text-emerald-700">
           Categoria 2
         </li>
       </ul>
