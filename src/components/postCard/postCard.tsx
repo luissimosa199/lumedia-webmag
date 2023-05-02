e43ba5fcc673f53b1fb210b79408a44680931218
Luis Simosa <simosa37@gmail.com>
2023-05-02 13:15:04 -0300

    feat: image on post list

--- src/components/postCard/postCard.tsx
+++ src/components/postCard/postCard.tsx
@@ -1,4 +1,5 @@
 import { type Post } from "@prisma/client";
+import Image from "next/image";
 import Link from "next/link";
 import { type FunctionComponent } from "react";
 
@@ -14,11 +15,16 @@ const PostCard: FunctionComponent<PostCardProps> = ({data}) => {
           return (
             <li
               key={post.id}
-              className="my-4 mx-3 grid h-64 grid-rows-2 border bg-slate-100 p-2"
+              className="my-4 mx-3 grid h-64 grid-rows-2 relative"
             >
-              <div className="row-start-2 flex flex-col self-end">
+              <div className="absolute top-0 w-full h-full z-0">
+              {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */}
+                <Image className="object-cover" fill alt="" src={`${(post.images[0] as string)}`} />
+              </div>
+
+              <div className="row-start-2 px-2 pb-2 pt-5 flex flex-col self-end z-10 bg-gradient-to-t from-black from-30% via-black via-60% to-transparent to-10% text-slate-100">
                 <div className="mb-2 flex items-center gap-2">
-                  <p className="mr-2 cursor-pointer rounded bg-yellow-300 px-2 py-[1px] text-sm uppercase transition-all hover:opacity-75">
+                  <p className="mr-2 cursor-pointer rounded bg-yellow-300 px-2 py-[1px] text-sm uppercase transition-all hover:opacity-75 text-black">
                     {post.tags[1]}
                   </p>
                   <p className="text-xs">
