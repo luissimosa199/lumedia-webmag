6b53698a7faba4b417d58b34f8eaf418ef6af354
Luis Simosa <simosa37@gmail.com>
2023-05-03 11:34:31 -0300

    feat: related articles new show image

--- src/components/postCardSmall/postCardSmall.tsx
+++ src/components/postCardSmall/postCardSmall.tsx
@@ -1,6 +1,7 @@
 import { type Post } from "@prisma/client";
 import Link from "next/link";
 import { type FunctionComponent } from "react";
+import Image from "next/image";
 
 interface PostCardSmallProps {
   post: Post;
@@ -9,12 +10,17 @@ interface PostCardSmallProps {
 const PostCardSmall: FunctionComponent<PostCardSmallProps> = ({ post }) => {
   return (
     <div className="mr-4 grid grid-cols-4 lg:grid-cols-3 justify-items-start">
-      <div className="h-24 w-24 border col-start-1 justify-self-center bg-slate-100"></div>
+
+      <div className="h-24 w-24 border col-start-1 justify-self-center bg-slate-100 relative">
+        <Image className="object-cover" fill alt="" src={`${(post?.images[0] as string)}`} />
+      </div>
+
       <div className="col-span-3 lg:col-span-2">
         <Link href={`/post/${post.id}`}>
           <h3 className="text-lg font-semibold">{post.title}</h3>
         </Link>
       </div>
+
     </div>
   );
 };
